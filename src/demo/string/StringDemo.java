e7cc24f38013d06084c88e0cd17b96693f152f01
Hui Yang <hui.yang.9045@gmail.com>
2025-03-29 17:50:24 -0400

    project setup

---
+++ src/demo/string/StringDemo.java
@@ -0,0 +1,11 @@
+package demo.string;
+
+import utilities.*;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		UtilityDemo.print("something");
+	}
+	
+}
---
+++ src/utilities/UtilityDemo.java
@@ -0,0 +1,9 @@
+package utilities;
+
+public class UtilityDemo {
+
+	public static void print(String input) {
+		System.out.println(input);
+	}
+	
+}
